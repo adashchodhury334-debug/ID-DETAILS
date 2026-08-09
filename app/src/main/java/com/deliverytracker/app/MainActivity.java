233eb51da80419e811fce201ee01a151cddc1d4a
adashchodhury334-debug <adashchodhury334@gmail.com>
2026-08-09 10:25:50 +0530

    Update MainActivity.java

--- app/src/main/java/com/deliverytracker/app/MainActivity.java
+++ app/src/main/java/com/deliverytracker/app/MainActivity.java
@@ -1,12 +1,12 @@
 package com.deliverytracker.app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
